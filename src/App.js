28f8fbb0bec22520e595317f390640b415111a8a
Yug KHOKHAR <83321546+kyr1234@users.noreply.github.com>
2021-09-23 07:41:22 +0000

    FGUYU

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React,{useState} from 'react';
 import './App.css';
-import Header from './Components/Header/Header.js';
-import Subnavbar from './Components/navbar/navbar.js';
-import Results from './Components/Results/Results.js';
+
 
 function App() {
 
-const [selectedoption,setselectedoption]=useState();
-
-
-
-
-
-  return (
-    <div className="App">
-
-
-<Header/>
-<Subnavbar setselectedoption={selectedoption}/>
-<Results selectedoption={selectedoption}/>
 
     </div>
   );
